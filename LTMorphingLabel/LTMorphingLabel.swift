709deed8749ee7ab7fe85caf6bd5d244464cfce5
Lex Tang <lexrus@gmail.com>
2014-08-10 11:19:08 +0800

    Fix #9.

--- LTMorphingLabel/LTMorphingLabel.swift
+++ LTMorphingLabel/LTMorphingLabel.swift
@@ -91,6 +91,7 @@ struct LTCharacterLimbo: DebugPrintable {
     
 }
 
+
 typealias LTMorphingStartClosure = (Void) -> Void
 typealias LTMorphingEffectClosure = (Character, index: Int, progress: Float) -> LTCharacterLimbo
 typealias LTMorphingDrawingClosure = LTCharacterLimbo -> Bool
@@ -105,6 +106,7 @@ typealias LTMorphingSkipFramesClosure = (Void) -> Int
 }
 
 
+// MARK: - LTMorphingLabel
 public class LTMorphingLabel: UILabel {
     
     public var morphingProgress: Float = 0.0
@@ -175,7 +177,7 @@ public class LTMorphingLabel: UILabel {
         }()
 }
 
-// Animation
+// MARK: - Animation extension
 extension LTMorphingLabel {
     
     func _displayFrameTick() {
@@ -370,7 +372,7 @@ extension LTMorphingLabel {
 }
 
 
-// Overrides
+// MARK: - Drawing extension
 extension LTMorphingLabel {
     override public func didMoveToSuperview() {
         if let s = self.text {
@@ -406,4 +408,117 @@ extension LTMorphingLabel {
             }
         }
     }
+}
+
+
+// MARK: - LTEmitterView
+
+struct LTEmitter {
+    
+    let layer: CAEmitterLayer = {
+        let _layer = CAEmitterLayer()
+        _layer.emitterPosition = CGPointMake(10, 10)
+        _layer.emitterSize = CGSizeMake(10, 1)
+        _layer.renderMode = kCAEmitterLayerOutline
+        _layer.emitterShape = kCAEmitterLayerLine
+        return _layer
+        }()
+    
+    let cell: CAEmitterCell = {
+        let image = UIImage(named:"Sparkle").CGImage
+        let _cell = CAEmitterCell()
+        _cell.name = "sparkle"
+        _cell.birthRate = 150.0
+        _cell.velocity = 50.0
+        _cell.velocityRange = -80.0
+        _cell.lifetime = 0.16
+        _cell.lifetimeRange = 0.1
+        _cell.emissionLongitude = CGFloat(M_PI_2 * 2.0)
+        _cell.emissionRange = CGFloat(M_PI_2 * 2.0)
+        _cell.contents = image
+        _cell.scale = 0.1
+        _cell.yAcceleration = 100
+        _cell.scaleSpeed = -0.06
+        _cell.scaleRange = 0.1
+        return _cell
+        }()
+    
+    var _duration: Float = 0.6
+    
+    init(name: String, duration: Float) {
+        cell.name = name
+        _duration = duration
+    }
+    
+    func play() {
+        if let cells = layer.emitterCells {
+            if cells.count > 0 {
+                return
+            }
+        }
+        
+        layer.emitterCells = [cell]
+        let d: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(_duration * Float(NSEC_PER_SEC)))
+        dispatch_after(d, dispatch_get_main_queue()) {
+            self.layer.birthRate = 0.0
+        }
+    }
+    
+    func stop() {
+        if layer.superlayer {
+            layer.removeFromSuperlayer()
+        }
+    }
+    
+    func update(configureClosure: LTEmitterConfigureClosure? = Optional.None) -> LTEmitter {
+        if let closure = configureClosure {
+            configureClosure!(self.layer, self.cell)
+        }
+        return self
+    }
+    
+}
+
+
+typealias LTEmitterConfigureClosure = (CAEmitterLayer, CAEmitterCell) -> Void
+
+
+class LTEmitterView: UIView {
+    
+    lazy var emitters: Dictionary<String, LTEmitter> = {
+        var _emitters = Dictionary<String, LTEmitter>()
+        return _emitters
+        }()
+    
+    func createEmitter(name: String, duration: Float, configureClosure: LTEmitterConfigureClosure? = Optional.None) -> LTEmitter {
+        var emitter: LTEmitter
+        if let e = emitterByName(name) {
+            emitter = e
+        } else {
+            emitter = LTEmitter(name: name, duration: duration)
+            
+            if let closure = configureClosure {
+                configureClosure!(emitter.layer, emitter.cell)
+            }
+            
+            layer.addSublayer(emitter.layer)
+            emitters.updateValue(emitter, forKey: name)
+        }
+        return emitter
+    }
+    
+    func emitterByName(name: String) -> LTEmitter? {
+        if let e = emitters[name] {
+            return e
+        }
+        return Optional.None
+    }
+    
+    func removeAllEmit() {
+        for (name, emitter) in emitters {
+            emitter.layer.removeFromSuperlayer()
+        }
+        emitters.removeAll(keepCapacity: false)
+    }
+    
 }
