84192b452e7a8b13559ae18a6fc10e33d8808639
Lex Tang <lexrus@gmail.com>
2015-01-08 18:33:03 +0800

    Add dynamic framework for iOS 8.

---
+++ LTMorphingLabel/LTMorphingLabel.h
@@ -0,0 +1,19 @@
+//
+//  LTMorphingLabel.h
+//  LTMorphingLabel
+//
+//  Created by Lex Tang on 1/8/15.
+//  Copyright (c) 2015 LexTang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LTMorphingLabel.
+FOUNDATION_EXPORT double LTMorphingLabelVersionNumber;
+
+//! Project version string for LTMorphingLabel.
+FOUNDATION_EXPORT const unsigned char LTMorphingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LTMorphingLabel/PublicHeader.h>
+
+
