fab14a813ed0c983ece069f5620ff48c43bee7d1
Lex Tang <lexrus@gmail.com>
2017-08-23 20:30:40 +0800

    Update copyright.

--- LTMorphingLabel/LTStringDiffResult.swift
+++ LTMorphingLabel/LTStringDiffResult.swift
@@ -3,7 +3,7 @@
 //  https://github.com/lexrus/LTMorphingLabel
 //
 //  The MIT License (MIT)
-//  Copyright (c) 2016 Lex Tang, http://lexrus.com
+//  Copyright (c) 2017 Lex Tang, http://lexrus.com
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a
 //  copy of this software and associated documentation files
